fb1f295272ea409828a9d126a7863a1d6f88ed9e
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-20 21:02:30 +0000

    Add a route and Create a page for rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+def play_rock
+  # redirect_to("https://www.wikipedia.org")
+  # render({ :plain => "Howdy, world!" })
+  # render({ :plain => rand(100) }) 
+  # render({ :html => "<h1>Hello World!</h1>".html_safe }) 
+  render({ :template => "game_templates/user_rock.html.erb" }) 
+end
+
 end
