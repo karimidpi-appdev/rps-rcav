c8475359a03b122a77574f18999b078b0fa9e9f1
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-20 22:13:19 +0000

    Creating home and paper pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,38 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-def play_rock
-  # redirect_to("https://www.wikipedia.org")
-  # render({ :plain => "Howdy, world!" })
-  # render({ :plain => rand(100) }) 
-  # render({ :html => "<h1>Hello World!</h1>".html_safe }) 
-  render({ :template => "game_templates/user_rock.html.erb" }) 
-end
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end
+
+  def play_rock
+    render({ :template => "game_templates/user_rock.html.erb" }) 
+  end
+
+  def play_paper
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+def play_rock
+  redirect_to("https://www.wikipedia.org")
+  render({ :plain => "Howdy, world!" })
+  render({ :plain => rand(100) }) 
+  render({ :html => "<h1>Hello World!</h1>".html_safe }) 
+  render({ :template => "game_templates/user_rock.html.erb" }) 
+end
+=end
